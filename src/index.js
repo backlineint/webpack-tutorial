d927c05e5eb27e472b850fbb90d5536fca7a0db3
Brian Perry <contact.brianperry@gmail.com>
2018-07-05 13:07:36 -0500

    Hot module replacement appendix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import component from './component';
 
-document.body.appendChild(component());
+let demoComponent = component();
 
-console.log('Hello World');
+document.body.appendChild(demoComponent);
+
+// HMR Interface
+if (module.hot) {
+  // Capture hot update
+  module.hot.accept('./component', () => {
+    const nextComponent = component();
+
+    // replace old component with the hot loaded one
+    document.body.replaceChild(nextComponent, demoComponent);
+
+    demoComponent = nextComponent;
+  });
+}
